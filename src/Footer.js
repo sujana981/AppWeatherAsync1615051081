b6d8b6c88b8ef825b59ff6c8f3a1627f7176ac7f
sujana981 <36873894+sujana981@users.noreply.github.com>
2018-03-27 00:15:10 +0800

    Add files via upload

---
+++ src/Footer.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import {
+  Text,
+  View
+} from 'react-native';
+
+const Footer = (props) => {
+  const { textStyle, backFooter } = styles;
+  return (
+    <View style={backFooter}>
+      <Text style={textStyle}>I Putu Sujana Atmaja</Text>
+    </View>
+  );
+};
+const styles = {
+    backFooter: {
+      backgroundColor: '#2E8B57',
+      justifyContent: 'center',
+      alignItems: 'center',
+      padding: 10,
+      paddingBottom: 10,
+      position: 'relative',
+    },
+    textStyle: {
+      fontSize: 18,
+      color: '#fff',
+      textAlign: 'center'
+    }
+}
+export default Footer;
